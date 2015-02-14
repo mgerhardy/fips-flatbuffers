07c533ed5c5f44032b9b679dae2f718f805a7fc4
Martin Gerhardy <martin.gerhardy@gmail.com>
2015-02-14 11:43:20 +0100

    * use pre compiled flatc

--- fips-generators/FlatC.py
+++ fips-generators/FlatC.py
@@ -11,13 +11,16 @@ from mod import log
 
 #-------------------------------------------------------------------------------
 def get_flatc_path() :
-    """find flatc compiler, fail if not exists"""
-    proj_path = os.path.dirname(os.path.abspath(__file__))
-    flatc_path = '{}/../flatbuffers/flatc'.format(proj_path)
-    flatc_path = os.path.normpath(flatc_path)
-    if not os.path.isfile(flatc_path) :
-        log.error("flatc executable not found")
-    return flatc_path
+    path = os.path.dirname(os.path.abspath(__file__))
+    if platform.system() == 'Windows' :
+        path += '/../tools/win32/'
+    elif platform.system() == 'Darwin' :
+        path += '/../tools/osx/'
+    elif platform.system() == 'Linux' :
+        path +=  '/../tools/linux/'
+    else :
+        error("Unknown host system {}".format(platform.system()))
+    return path + 'flatc'
 
 #-------------------------------------------------------------------------------
 def run_flatc(input_file, out_hdr) :
@@ -39,5 +42,6 @@ def generate(input_file, out_src, out_hdr) :
     :param out_hdr:     path for output header files
     """
     flatc_path = get_flatc_path()
+    print(flatc_path)
     run_flatc(input_file, out_hdr)
 
